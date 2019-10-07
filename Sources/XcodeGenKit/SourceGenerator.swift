efdc666525db94b5f84c76fd4c2a36002976e08c
giginet <giginet.net@gmail.com>
2019-10-07 10:30:52 +0900

    WIP

--- Sources/XcodeGenKit/SourceGenerator.swift
+++ Sources/XcodeGenKit/SourceGenerator.swift
@@ -289,11 +289,16 @@ class SourceGenerator {
             let groupPath = isTopLevelGroup ?
                 ((try? path.relativePath(from: project.basePath)) ?? path).string :
                 path.lastComponent
+            let projectPath = Path("/Users/giginet/.ghq/github.com/yonaskolb/XcodeGen")
+            print(groupPath)
+            print(projectPath)
+            print(path)
+            let relativePath = try? path.relativePath(from: projectPath)
             let group = PBXGroup(
                 children: children,
                 sourceTree: .group,
                 name: groupName != groupPath ? groupName : nil,
-                path: groupPath
+                path: relativePath?.string ?? path.string
             )
             groupReference = addObject(group)
             groupsByPath[path] = groupReference
